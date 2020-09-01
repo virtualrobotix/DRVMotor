69d5a0d9584bb8fd52c4244995083122e5a181aa
LukeMike <luca.micheletti.laser@gmail.com>
2020-09-01 15:11:23 +0200

    ROVMixer_Alitrak: added new input to select velocity mode

--- ROVMixer_Alitrak.ino
+++ ROVMixer_Alitrak.ino
@@ -1,24 +1,30 @@
 #include <DRV8835MotorShield.h>
 
 #define LED_PIN 13
-#define MODE_PIN 7
+#define VELMODE_PIN 2
 
 #define CH3_IN_PIN 22
 #define CH1_IN_PIN 23
+#define VELMODE_IN_PIN 21
 
 #define CH3_FLAG 1
 #define CH1_FLAG 2
+#define VELMODE_FLAG 4
 
+#define CHANNEL_MIN 1100
+#define CHANNEL_MAX 1900
 #define CHANNEL_TRIM 1500
-#define CHANNEL_DZ 50
+#define CHANNEL_DZ 30
 
 volatile uint8_t bUpdateFlagsShared;
 
 volatile uint16_t unCH3InShared;
 volatile uint16_t unCH1InShared;
+volatile uint16_t unVELMODEInShared;
 
 uint32_t ulCH3Start;
 uint32_t ulCH1Start;
+uint32_t ulVELMODEStart;
 
 DRV8835MotorShield motors;
 
@@ -35,13 +41,17 @@ void calcCH3()
     // else it must be a falling edge, so lets get the time and subtract the time of the rising edge
     // this gives use the time between the rising and falling edges i.e. the pulse duration.
     unCH3InShared = (uint16_t)(micros() - ulCH3Start);
-    if(unCH3InShared > 1900)
+    if(unCH3InShared > CHANNEL_MAX)
     {
-      unCH3InShared = 1900;     
+      unCH3InShared = CHANNEL_MAX;     
     }
-    else if(unCH3InShared < 1100)
+    else if((unCH3InShared > (CHANNEL_TRIM - CHANNEL_DZ)) && (unCH3InShared < (CHANNEL_TRIM + CHANNEL_DZ)))
     {
-      unCH3InShared = 1100;     
+      unCH3InShared = CHANNEL_TRIM;     
+    }
+    else if(unCH3InShared < CHANNEL_MIN)
+    {
+      unCH3InShared = CHANNEL_MIN;     
     }
     // use set the CH3 flag to indicate that a new CH3 signal has been received
     bUpdateFlagsShared |= CH3_FLAG;
@@ -61,16 +71,46 @@ void calcCH1()
     // else it must be a falling edge, so lets get the time and subtract the time of the rising edge
     // this gives use the time between the rising and falling edges i.e. the pulse duration.
     unCH1InShared = (uint16_t)(micros() - ulCH1Start);
-    if(unCH1InShared > 1900)
+    if(unCH1InShared > CHANNEL_MAX)
     {
-      unCH1InShared = 1900;     
+      unCH1InShared = CHANNEL_MAX;     
     }
-    else if(unCH1InShared < 1100)
+    else if((unCH1InShared > (CHANNEL_TRIM - CHANNEL_DZ)) && (unCH1InShared < (CHANNEL_TRIM + CHANNEL_DZ)))
     {
-      unCH1InShared = 1100;     
+      unCH1InShared = CHANNEL_TRIM;     
+    }
+    else if(unCH1InShared < CHANNEL_MIN)
+    {
+      unCH1InShared = CHANNEL_MIN;     
     }
     // use set the CH3 flag to indicate that a new CH3 signal has been received
     bUpdateFlagsShared |= CH1_FLAG;
+  }
+}
+
+// simple interrupt service routine
+void calcVELMODE()
+{
+  // if the pin is high, its a rising edge of the signal pulse, so lets record its value
+  if(digitalRead(VELMODE_IN_PIN) == HIGH)
+  {
+    ulVELMODEStart = micros();
+  }
+  else
+  {
+    // else it must be a falling edge, so lets get the time and subtract the time of the rising edge
+    // this gives use the time between the rising and falling edges i.e. the pulse duration.
+    unVELMODEInShared = (uint16_t)(micros() - ulVELMODEStart);
+    if(unVELMODEInShared > CHANNEL_MAX)
+    {
+      unVELMODEInShared = CHANNEL_MAX;     
+    }
+    else if(unVELMODEInShared < CHANNEL_MIN)
+    {
+      unVELMODEInShared = CHANNEL_MIN;     
+    }
+    // use set the VELMODE flag to indicate that a new VELMODE signal has been received
+    bUpdateFlagsShared |= VELMODE_FLAG;
   }
 }
 
@@ -82,11 +122,12 @@ void setup()
  
   Serial.println("multiChannels");
 
-  pinMode(MODE_PIN, OUTPUT);
-  digitalWrite(MODE_PIN, HIGH);
+  pinMode(VELMODE_PIN, OUTPUT);
+  digitalWrite(VELMODE_PIN, LOW);
 
   attachInterrupt(CH3_IN_PIN, calcCH3,CHANGE);
   attachInterrupt(CH1_IN_PIN, calcCH1,CHANGE);
+  attachInterrupt(VELMODE_IN_PIN, calcVELMODE,CHANGE);
 
   // uncomment one or both of the following lines if your motors' directions need to be flipped
   //motors.flipM1(true);
@@ -97,6 +138,7 @@ void loop()
 {
   static uint16_t unCH3In;
   static uint16_t unCH1In;
+  static uint16_t unVELMODEIn;
 
   static int Motor1Speed;
   static int Motor2Speed;
@@ -119,6 +161,11 @@ void loop()
       unCH1In = unCH1InShared;
     }
    
+    if(bUpdateFlags & VELMODE_FLAG)
+    {
+      unVELMODEIn = unVELMODEInShared;
+    }
+   
     bUpdateFlagsShared = 0;
    
     interrupts();
@@ -128,7 +175,7 @@ void loop()
   {
     digitalWrite(LED_PIN, HIGH);
     
-    Motor1Speed = unCH3In - 1500;
+    Motor1Speed = unCH3In - CHANNEL_TRIM;
     
     Serial.print("servoCH3: ");
     Serial.print(unCH3In);
@@ -146,7 +193,7 @@ void loop()
   {
     digitalWrite(LED_PIN, HIGH);
     
-    Motor2Speed = unCH1In - 1500;
+    Motor2Speed = unCH1In - CHANNEL_TRIM;
     
     Serial.print("servoCH1: ");
     Serial.print(unCH1In);
@@ -156,6 +203,29 @@ void loop()
         
     motors.setM2Speed(Motor2Speed);
     delay(2);
+    
+    digitalWrite(LED_PIN, LOW);
+  }
+ 
+  if(bUpdateFlags & VELMODE_FLAG)
+  {
+    digitalWrite(LED_PIN, HIGH);
+    
+    Serial.print("servoVELMODE: ");
+    Serial.print(unVELMODEIn);
+    Serial.print(" - ");
+        
+    if (unVELMODEIn < CHANNEL_TRIM)
+    {
+      digitalWrite(VELMODE_PIN, LOW);
+      Serial.print("Slow");
+    }
+    else
+    {
+      digitalWrite(VELMODE_PIN, HIGH);
+      Serial.print("Fast");
+    }
+    Serial.println("");
     
     digitalWrite(LED_PIN, LOW);
   }
