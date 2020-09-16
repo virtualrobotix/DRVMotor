e7918dd3de971a2be99103d23e1db48fa242db5c
LukeMike <luca.micheletti.laser@gmail.com>
2020-09-16 13:23:51 +0200

    ROVMixer: added PWM In (pin 20) to reset Motor Controller (digit pin 4)

--- ROVMixer_Alitrak.ino
+++ ROVMixer_Alitrak.ino
@@ -2,14 +2,17 @@
 
 #define LED_PIN 13
 #define VELMODE_PIN 2
+#define RESET_PIN 4
 
 #define CH3_IN_PIN 22
 #define CH1_IN_PIN 23
 #define VELMODE_IN_PIN 21
+#define RESET_IN_PIN 20
 
 #define CH3_FLAG 1
 #define CH1_FLAG 2
 #define VELMODE_FLAG 4
+#define RESET_FLAG 8
 
 #define CHANNEL_MIN 1100
 #define CHANNEL_MAX 1900
@@ -21,10 +24,12 @@ volatile uint8_t bUpdateFlagsShared;
 volatile uint16_t unCH3InShared;
 volatile uint16_t unCH1InShared;
 volatile uint16_t unVELMODEInShared;
+volatile uint16_t unRESETInShared;
 
 uint32_t ulCH3Start;
 uint32_t ulCH1Start;
 uint32_t ulVELMODEStart;
+uint32_t ulRESETStart;
 
 DRV8835MotorShield motors;
 
@@ -114,6 +119,32 @@ void calcVELMODE()
   }
 }
 
+// simple interrupt service routine
+void calcRESET()
+{
+  // if the pin is high, its a rising edge of the signal pulse, so lets record its value
+  if(digitalRead(RESET_IN_PIN) == HIGH)
+  {
+    ulRESETStart = micros();
+  }
+  else
+  {
+    // else it must be a falling edge, so lets get the time and subtract the time of the rising edge
+    // this gives use the time between the rising and falling edges i.e. the pulse duration.
+    unRESETInShared = (uint16_t)(micros() - ulRESETStart);
+    if(unRESETInShared > CHANNEL_MAX)
+    {
+      unRESETInShared = CHANNEL_MAX;     
+    }
+    else if(unRESETInShared < CHANNEL_MIN)
+    {
+      unRESETInShared = CHANNEL_MIN;     
+    }
+    // use set the RESET flag to indicate that a new RESET signal has been received
+    bUpdateFlagsShared |= RESET_FLAG;
+  }
+}
+
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
@@ -125,9 +156,13 @@ void setup()
   pinMode(VELMODE_PIN, OUTPUT);
   digitalWrite(VELMODE_PIN, LOW);
 
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, LOW);
+
   attachInterrupt(CH3_IN_PIN, calcCH3,CHANGE);
   attachInterrupt(CH1_IN_PIN, calcCH1,CHANGE);
   attachInterrupt(VELMODE_IN_PIN, calcVELMODE,CHANGE);
+  attachInterrupt(RESET_IN_PIN, calcRESET,CHANGE);
 
   // uncomment one or both of the following lines if your motors' directions need to be flipped
   //motors.flipM1(true);
@@ -139,6 +174,7 @@ void loop()
   static uint16_t unCH3In;
   static uint16_t unCH1In;
   static uint16_t unVELMODEIn;
+  static uint16_t unRESETIn;
 
   static int Motor1Speed;
   static int Motor2Speed;
@@ -166,6 +202,11 @@ void loop()
       unVELMODEIn = unVELMODEInShared;
     }
    
+    if(bUpdateFlags & RESET_FLAG)
+    {
+      unRESETIn = unRESETInShared;
+    }
+    
     bUpdateFlagsShared = 0;
    
     interrupts();
@@ -224,6 +265,29 @@ void loop()
     {
       digitalWrite(VELMODE_PIN, HIGH);
       Serial.print("Fast");
+    }
+    Serial.println("");
+    
+    digitalWrite(LED_PIN, LOW);
+  }
+ 
+  if(bUpdateFlags & RESET_FLAG)
+  {
+    digitalWrite(LED_PIN, HIGH);
+    
+    Serial.print("servoRESET: ");
+    Serial.print(unRESETIn);
+    Serial.print(" - ");
+        
+    if (unRESETIn < CHANNEL_TRIM)
+    {
+      digitalWrite(RESET_PIN, LOW);
+      Serial.print("Restore");
+    }
+    else
+    {
+      digitalWrite(RESET_PIN, HIGH);
+      Serial.print("Reset");
     }
     Serial.println("");
     
